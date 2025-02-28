d67043f5e9677bd8bbf2c80c681d9a5f5a70a36b
Juan Carlos Zuluaga Cardona <jzuluaga@planatechnologies.com>
2025-02-27 20:34:19 -0500

    Add base comission employee

---
+++ POOConcepts/POOConcepts/BaseComissionEmployee.cs
@@ -0,0 +1,34 @@
+﻿namespace POOConcepts;
+
+public class BaseComissionEmployee : ComissionEmployee
+{
+    private decimal _salary;
+
+    public BaseComissionEmployee(int id, string firstName, string lastName, bool isActive, Date? bornDate, Date? hireDate, float comissionPercentaje, decimal sales, decimal salary) : base(id, firstName, lastName, isActive, bornDate, hireDate, comissionPercentaje, sales)
+    {
+        Salary = salary;
+    }
+
+    public decimal Salary { get => _salary; set => _salary = ValidateSalary(value); }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()}\n\t" +
+            $"Salary Base....: {Salary,20:C2}\n\t" +
+            $"Value to pay...: {GetValueToPay(),20:C2}";
+    }
+
+    public override decimal GetValueToPay()
+    {
+        return base.GetValueToPay() + Salary;
+    }
+
+    private decimal ValidateSalary(decimal salary)
+    {
+        if (salary < 500000)
+        {
+            throw new Exception($"The salary: {salary:C2}, is less than the $500,000.00.");
+        }
+        return salary;
+    }
+}
--- POOConcepts/POOConcepts/Program.cs
+++ POOConcepts/POOConcepts/Program.cs
@@ -7,8 +7,9 @@ try
     var washer = new HourlyEmployee(3, "Jaime", "Betancur", true, new Date(1980, 12, 1), new Date(2020, 4, 20), 178, 10250.56M);
     var salesPerson1 = new ComissionEmployee(4, "Monica", "Gil", true, new Date(1989, 7, 1), new Date(2020, 4, 20), 0.03F, 670000000M);
     var salesPerson2 = new ComissionEmployee(5, "Henry", "Vasquez", true, new Date(1979, 1, 1), new Date(2020, 4, 20), 0.03F, 59000000M);
+    var salesPerson3 = new BaseComissionEmployee(6, "Patrica", "Rodriguez", true, new Date(1990, 2, 23), new Date(2025, 1, 10), 0.015F, 97000000M, 650000M);
 
-    var employees = new List<Employee>() { secretary, manager, washer, salesPerson1, salesPerson2 };
+    var employees = new List<Employee>() { secretary, manager, washer, salesPerson1, salesPerson2, salesPerson3 };
     var total = 0M;
     foreach (var employee in employees)
     {
