684d616bbea19084226eaceafed2d6e7bc7f3aec
Juan Carlos Zuluaga Cardona <jzuluaga@planatechnologies.com>
2025-02-27 20:23:25 -0500

    Add comission employee

---
+++ POOConcepts/POOConcepts/ComissionEmployee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOConcepts;
+
+public class ComissionEmployee : Employee
+{
+    private float _comissionPercentaje;
+    private decimal _sales;
+
+    public ComissionEmployee(int id, string firstName, string lastName, bool isActive, Date? bornDate, Date? hireDate, float comissionPercentaje, decimal sales) : base(id, firstName, lastName, isActive, bornDate, hireDate)
+    {
+        ComissionPercentaje = comissionPercentaje;
+        Sales = sales;
+    }
+
+    public float ComissionPercentaje { get => _comissionPercentaje; set => _comissionPercentaje = ValidateComissionPercentaje(value); }
+    public decimal Sales { get => _sales; set => _sales = ValidateSales(value); }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()}\n\t" +
+            $"Commision %....: {ComissionPercentaje,20:P2}\n\t" +
+            $"Sales..........: {Sales,20:C2}\n\t" +
+            $"Value to pay...: {GetValueToPay(),20:C2}";
+    }
+
+    public override decimal GetValueToPay()
+    {
+        return (decimal)_comissionPercentaje * _sales;
+    }
+
+    private float ValidateComissionPercentaje(float comissionPercentaje)
+    {
+        if (comissionPercentaje < 0 || comissionPercentaje > 0.3)
+        {
+            throw new Exception($"The comission percentaje: {comissionPercentaje:P2}, is not valid.");
+        }
+        return comissionPercentaje;
+    }
+
+    private decimal ValidateSales(decimal sales)
+    {
+        if (sales < 0)
+        {
+            throw new Exception($"The sales: {sales:C2}, is less than $0.00.");
+        }
+        return sales;
+    }
+}
--- POOConcepts/POOConcepts/Program.cs
+++ POOConcepts/POOConcepts/Program.cs
@@ -5,8 +5,10 @@ try
     var secretary = new SalaryEmployee(1, "Sandra", "Martinez", true, new Date(1981, 11, 14), new Date(2022, 12, 14), 2500000M);
     var manager = new SalaryEmployee(2, "Fabio", "Ochoa", true, new Date(1971, 11, 14), new Date(2002, 12, 14), 11756324.15M);
     var washer = new HourlyEmployee(3, "Jaime", "Betancur", true, new Date(1980, 12, 1), new Date(2020, 4, 20), 178, 10250.56M);
+    var salesPerson1 = new ComissionEmployee(4, "Monica", "Gil", true, new Date(1989, 7, 1), new Date(2020, 4, 20), 0.03F, 670000000M);
+    var salesPerson2 = new ComissionEmployee(5, "Henry", "Vasquez", true, new Date(1979, 1, 1), new Date(2020, 4, 20), 0.03F, 59000000M);
 
-    var employees = new List<Employee>() { secretary, manager, washer };
+    var employees = new List<Employee>() { secretary, manager, washer, salesPerson1, salesPerson2 };
     var total = 0M;
     foreach (var employee in employees)
     {
